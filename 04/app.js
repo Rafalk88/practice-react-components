c70826a411469532045741add99706423e8ba15d
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-08-22 17:39:56 +0200

    eslint errors fixed

--- 04/app.js
+++ 04/app.js
@@ -1,87 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-    state = { 
+  state = {
+    firstName: '',
+    lastName: '',
+    searchQuery: '',
+    users: ['Jan Kowalski', 'Michał Nowak']
+  }
+
+  renderUsersList () {
+    const { users } = this.state
+    return users.map((name, i) => {
+      return (
+        <li
+          onClick={this.clickHandler}
+          key={i}
+        >
+          { name }
+        </li>
+      )
+    })
+  }
+
+  clickHandler = e => {
+    const { innerText: userName } = e.target
+    this.removeUser(userName)
+  }
+
+  inputChange = e => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render () {
+    const { firstName, lastName } = this.state
+    return (
+      <section onSubmit={this.submitHandler}>
+        <form>
+          <input
+            name={'firstName'}
+            value={firstName}
+            onChange={this.inputChange}
+          />
+          <input
+            name={'lastName'}
+            value={lastName}
+            onChange={this.inputChange}
+          />
+          <input type={'submit'}/>
+        </form>
+        <ul>{ this.renderUsersList() }</ul>
+      </section>
+    )
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+
+    const { firstName, lastName } = this.state
+    if (firstName && lastName) {
+      this.addUser(`${firstName} ${lastName}`)
+      this.setState({
         firstName: '',
-        lastName: '',
-        searchQuery: '',
-        users: ['Jan Kowalski', 'Michał Nowak'],
+        lastName: ''
+      })
+    } else {
+      // tutaj komunikat dla użytkownika
     }
+  }
 
-    renderUsersList() {
-        const {users} = this.state;
-        return users.map(name => {
-            return (
-                <li onClick={ this.clickHandler }>
-                    { name }
-                </li>
-            );
-        });
-    }
+  addUser (name) {
+    this.setState({
+      users: [...this.state.users, name]
+    })
+  }
 
-    clickHandler = e => {
-        const {innerText: userName} = e.target;
-        this.removeUser(userName);
-    }
+  removeUser (name) {
+    const currUsers = this.state.users.filter(
+      user => user !== name
+    )
 
-    inputChange = e => {
-        const {name, value} = e.target;
-        this.setState({
-            [name]: value,
-        });
-    }
-
-    render() {
-        const { firstName, lastName } = this.state;
-        return (
-            <section onSubmit={ this.submitHandler }>
-                <form>
-                    <input name="firstName"
-                        value={ firstName }
-                        onChange={ this.inputChange }
-                    />
-                    <input name="lastName"
-                        value={ lastName }
-                        onChange={ this.inputChange }
-                    />
-                    <input type="submit"/>
-                </form>
-                <ul>{ this.renderUsersList() }</ul>
-            </section>
-        );
-    }
-
-    submitHandler = e => {
-        e.preventDefault();
-
-        const { firstName, lastName } = this.state;
-        if(firstName && lastName) {
-            this.addUser(`${firstName} ${lastName}`);
-            this.setState({
-                firstName: '',
-                lastName: '',
-            });
-        } else {
-            // tutaj komunikat dla użytkownika
-        }
-    }
-
-    addUser(name) {
-        this.setState({
-            users: [...this.state.users, name],
-        });
-    }
-
-    removeUser(name) {
-        const currUsers = this.state.users.filter(
-            user => user != name
-        );
-
-        this.setState({
-            users: currUsers,
-        });
-    }
+    this.setState({
+      users: currUsers
+    })
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'))
