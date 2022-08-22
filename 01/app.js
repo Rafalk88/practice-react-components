fe33b07aa9787d36bd79e8552366324f0cbdd6b7
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-08-22 16:21:39 +0200

    setInterval for counter added

--- 01/app.js
+++ 01/app.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
 class App extends React.Component {
   state = {
     counter: 0
   }
 
-  render () {
+  render() {
     console.log('render')
 
     return <h1>{this.state.counter}</h1>
   }
 
-  componentDidMount () {
+  componentDidMount() {
     console.log('.componentDidMount')
+    const { time } = this.props
+    this.id = setInterval(
+      () => {
+        const { counter } = this.state
+        this.setState({ counter: counter + 1 })
+      }
+      , time
+    )
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+App.proptypes = {
+  time: PropTypes.number
+}
+
+ReactDOM.render(<App time={5000} />, document.querySelector('#root'))
